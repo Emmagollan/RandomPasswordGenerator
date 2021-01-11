bf04a55159afca7b7a99863050308820ad830272
BBT <emmagollan33@gmail.com>
2021-01-11 18:15:57 -0500

    add prompts

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  window.prompt("Would you like you password to be between 8 - 128 characters?")
+  window.prompt("Would you like numbers in your password?")
+  window.prompt("would you like uppercase letters in your password?")
+  window.prompt("would you like lowercase letters in your password?")
+  window.prompt("would you like special characters in your password?")
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
